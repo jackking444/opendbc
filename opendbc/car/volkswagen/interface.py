cfc4b79c28955e7bc3d8f5d011f8a7ac57a06cfa
jackking444 <sitnikove@gmail.com>
2025-06-17 14:16:31 +0300

    non_linear torque support

--- opendbc/car/volkswagen/interface.py
+++ opendbc/car/volkswagen/interface.py
@@ -1,3 +1,8 @@
+from cereal import car
+from panda import Panda
+from math import exp
+from openpilot.common.params import Params
+#from openpilot.selfdrive.car import get_safety_config, get_friction
 from opendbc.car import get_friction,get_safety_config, structs
 from opendbc.car.interfaces import TorqueFromLateralAccelCallbackType, FRICTION_THRESHOLD, LatControlInputs,CarInterfaceBase
 from opendbc.car.volkswagen.carcontroller import CarController
@@ -8,31 +13,40 @@ NON_LINEAR_TORQUE_PARAMS = {
    CAR.VOLKSWAGEN_SHARAN_MK2: [119.999999999518543, 0.013752973175158755, 0.29000000000000004, -0.007255929913027823, \
                               15.000000000038696, 1.1156542774105294, 0.10000000000000002, 0.9999999999999999]  #khonsu's JSW + back in my day...
  }
+NON_LINEAR_TORQUE_PARAMS = {
+  CAR.VOLKSWAGEN_SHARAN_MK2: [119.999999999518543, 0.013752973175158755, 0.29000000000000004, -0.007255929913027823, \
+                             15.000000000038696, 1.1156542774105294, 0.10000000000000002, 0.9999999999999999]  #khonsu's JSW + back in my day...
+}
 class CarInterface(CarInterfaceBase):
-  def torque_from_lateral_accel_siglin(self, latcontrol_inputs: LatControlInputs, torque_params: car.CarParams.LateralTorqueTuning, lateral_accel_error: float,
-                                        lateral_accel_deadzone: float, friction_compensation: bool, gravity_adjusted: bool) -> float:
-     friction = get_friction(lateral_accel_error, lateral_accel_deadzone, FRICTION_THRESHOLD, torque_params, friction_compensation)
-
-     def sig(val):
-       # https://timvieira.github.io/blog/post/2014/02/11/exp-normalize-trick
-       if val >= 0:
-         return 1 / (1 + exp(-val)) - 0.5
-       else:
-         z = exp(val)
-         return z / (1 + z) - 0.5
-
-     # The "lat_accel vs torque" relationship is assumed to be the sum of "sigmoid + linear" curves
-     # An important thing to consider is that the slope at 0 should be > 0 (ideally >1)
-     # This has big effect on the stability about 0 (noise when going straight)
-     # ToDo: To generalize to other VWs, explore tanh function as the nonlinear
-     non_linear_torque_params = NON_LINEAR_TORQUE_PARAMS.get(self.CP.carFingerprint)
-     assert non_linear_torque_params, "The params are not defined"
-     a, b, c, _ = non_linear_torque_params
-     steer_torque = (sig(latcontrol_inputs.lateral_acceleration * a) * b) + (latcontrol_inputs.lateral_acceleration * c)
-     return float(steer_torque) + friction
-
   CarState = CarState
   CarController = CarController
+  def torque_from_lateral_accel_siglin(self, latcontrol_inputs: LatControlInputs, torque_params: car.CarParams.LateralTorqueTuning, lateral_accel_error: float,
+                                       lateral_accel_deadzone: float, friction_compensation: bool, gravity_adjusted: bool) -> float:
+    friction = get_friction(lateral_accel_error, lateral_accel_deadzone, FRICTION_THRESHOLD, torque_params, friction_compensation)
+
+    def sig(val):
+      # https://timvieira.github.io/blog/post/2014/02/11/exp-normalize-trick
+      if val >= 0:
+        return 1 / (1 + exp(-val)) - 0.5
+      else:
+        z = exp(val)
+        return z / (1 + z) - 0.5
+
+    # The "lat_accel vs torque" relationship is assumed to be the sum of "sigmoid + linear" curves
+    # An important thing to consider is that the slope at 0 should be > 0 (ideally >1)
+    # This has big effect on the stability about 0 (noise when going straight)
+    # ToDo: To generalize to other VWs, explore tanh function as the nonlinear
+    non_linear_torque_params = NON_LINEAR_TORQUE_PARAMS.get(self.CP.carFingerprint)
+    assert non_linear_torque_params, "The params are not defined"
+    a, b, c, _, e, f, g, h = non_linear_torque_params
+
+    speed_factor = (40.23 / (max(1.0, latcontrol_inputs.vego + e))**f)
+    speed_factor2 = max(0.2, 40.23 / (max(1.0, latcontrol_inputs.vego + g))**h)
+    steer_torque = (sig(latcontrol_inputs.lateral_acceleration * a * speed_factor) * b * speed_factor2) + (latcontrol_inputs.lateral_acceleration * c)
+
+    return float(steer_torque) + friction
+
+
 
   def torque_from_lateral_accel(self) -> TorqueFromLateralAccelCallbackType:
      if self.CP.carFingerprint in NON_LINEAR_TORQUE_PARAMS:
@@ -92,13 +106,12 @@ class CarInterface(CarInterfaceBase):
 
     ret.steerLimitTimer = 0.4
     if ret.flags & VolkswagenFlags.PQ:
-      ret.steerActuatorDelay = 0.11
-      ret.longitudinalTuning.kf = 1.2
-      ret.longitudinalTuning.kpBP = [0.]
-      ret.longitudinalTuning.kpV =  [.45]
-      ret.longitudinalTuning.kiBP = [0.]
-      ret.longitudinalTuning.kiV =  [.69]
-      ret.longitudinalActuatorDelay = 0.6
+      ret.steerActuatorDelay = 0.12
+      ret.lateralTuning.pid.kpBP = [0.0]
+      ret.lateralTuning.pid.kiBP = [0.0, 41.0]
+      ret.lateralTuning.pid.kf = 0.000003
+      ret.lateralTuning.pid.kpV = [0.01]
+      ret.lateralTuning.pid.kiV = [0.02, 0.2]
       CarInterfaceBase.configure_torque_tune(candidate, ret.lateralTuning)
     else:
       ret.steerActuatorDelay = 0.1
